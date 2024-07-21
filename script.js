cf103bf38564c9cf3da59a13c64e75da3d484d98
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-07-21 19:14:44 +0300

    uploading files

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded" , function() {
+    // select elements----
+    const addButton = document.getElementById("add-task-btn"),
+    taskInput = document.getElementById("task-input"),
+    taskList = document.getElementById("task-list");
+
+    addButton.addEventListener("click" , () => addTask());
+
+    taskInput.addEventListener("keypress" , function (event) {
+        if(event.key === "Enter") {
+            addTask();
+        }
+    });
+
+
+    function addTask() {
+        let taskText = taskInput.value.trim();
+
+        // check on value---
+        if(taskText){
+            // create li element
+            let li = document.createElement("li");
+            li.textContent = taskText;
+            // create a btn to remove the task--
+            let removeBtn = document.createElement("button");
+            removeBtn.textContent = "Remove";
+            removeBtn.classList.add("remove-btn");
+            removeBtn.onclick = () => li.remove();
+            li.append(removeBtn);
+            taskList.append(li);
+            // clear value--
+            taskInput.value = "";
+        }
+        else {
+            alert("please add a task");
+        }
+    };
+
+    addTask();
+});
+
